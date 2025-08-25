d3d13bc1070c2e09e21221a3f914623fea0165b0
Dilani.Hewage <hdakanchana@gmail.com>
2025-08-25 14:49:32 +0530

    login frontend
    login frontend

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import './App.css';
 import Registration from './pages/Registration';
 import StudentDashboard from './pages/Student/StudentDashboard';
 import TeacherDashboard from './pages/Teacher/TeacherDashboard';
+import Login from './pages/Login';
+
 
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Routes>
-          <Route path="" element={<Registration />} />
-          <Route path='/student-dashboard' element={<StudentDashboard/>}/>
-          <Route path='/teacher-dashboard' element={<TeacherDashboard/>}/>
-        </Routes>
-      </div>
-    </Router>
-  );
+    return ( 
+    < Router >
+        <div className = "min-h-screen bg-gray-50" >
+           <Routes >
+                <Route path = "" element = { < Registration /> }/> 
+
+                <Route path = '/student-dashboard' element = { < StudentDashboard /> }/>
+
+                <Route path = '/teacher-dashboard' element = { < TeacherDashboard /> }/>
+
+                <Route path="/login" element={<Login />} />
+
+            </Routes > 
+
+        </div> 
+    </Router >
+
+        )
 }
 
 export default App;
